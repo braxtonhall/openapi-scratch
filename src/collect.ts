fe88ee6bd09681e3d488c4d17f879b14ea6adb06
braxtonhall <braxtonjhall@gmail.com>
2023-03-04 19:30:12 +0100

    More concise type checking

--- src/collect.ts
+++ src/collect.ts
@@ -1,4 +1,9 @@
-export const collect = (container: object, searchKey: string): unknown[] => {
+export const collect = (container: object, searchKey: string): unknown[] =>
+	deduplicate(collectWithDuplicates(container, searchKey));
+
+const deduplicate = <T>(values: T[]): T[] => [...new Set(values)];
+
+const collectWithDuplicates = (container: object, searchKey: string): unknown[] => {
 	try {
 		return Object.entries(container).flatMap(([key, value]) => {
 			if (key === searchKey) {
--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -1,11 +1,25 @@
 import {Request as HapiRequest} from '@hapi/hapi'
 import { components, operations, paths } from "../generated";
+import { Context } from "openapi-backend";
+import * as Hapi from "@hapi/hapi";
 
 
 type Request<Op extends keyof operations> = HapiRequest & {
 	params: operations[Op] extends { parameters: { query: infer T } } ? T : never;
 	payload: operations[Op] extends { requestBody: { content: { "application/json": infer T } } } ? T : never;
 	headers: operations[Op] extends { parameters: { header: infer T } } ? T : never;
+}
+
+// TODO get return type information!
+type Handler<Op extends keyof operations> = (
+	context: Context,
+	req: Request<Op>,
+	h?: Hapi.ResponseToolkit,
+	err?: Error,
+) => Promise<unknown>;
+
+type HandlerCollection = {
+	[Op in keyof operations]?: Handler<Op>;
 }
 
 /**
@@ -43,7 +57,7 @@ type Request<Op extends keyof operations> = HapiRequest & {
  *             schema:
  *               $ref: "#/components/schemas/Error"
  */
-export const listPets = (req: Request<"listPets">) => {
+export const listPets: Handler<"listPets"> = async (ctx, req) => {
 	return "listPets";
 };
 
@@ -76,6 +90,12 @@ export const listPets = (req: Request<"listPets">) => {
  *             schema:
  *               $ref: "#/components/schemas/Error"
  */
-export const makePet = (req: Request<"makePet">) => {
+export const makePet: Handler<"makePet"> = async (context, req) => {
 	return "makePet";
+};
+
+// Alternative export style for more concise typechecking!
+export const pets: HandlerCollection = {
+	makePet,
+	listPets,
 };
