fbe38ea17853b93919a0dcc054cc7e0337f4eaad
braxtonhall <braxtonjhall@gmail.com>
2023-03-06 13:30:17 +0100

    Move types into their own module

--- src/routes/pets.ts
+++ src/routes/pets.ts
@@ -1,34 +1,4 @@
-import { operations } from "../generated";
-import { Context } from "openapi-backend";
-import * as Hapi from "@hapi/hapi";
-
-type Request<Op extends keyof operations> = Omit<Hapi.Request, 'params' | 'payload' | 'headers'> & {
-	params: operations[Op] extends { parameters: { query: infer T } } ? T : never;
-	payload: operations[Op] extends { requestBody: { content: { "application/json": infer T } } } ? T : never;
-	headers: operations[Op] extends { parameters: { header: infer T } } ? T : never;
-}
-
-type PickContent<T> = T extends { content: Record<string, infer C> }
-	? C
-	: T extends Record<string, infer C>
-		? PickContent<C>
-		: never;
-
-type ResponseToolkit<Op extends keyof operations> = Omit<Hapi.ResponseToolkit, "response"> & {
-	response(value?: PickContent<operations[Op]>): Hapi.ResponseObject
-};
-
-// TODO get return type information!
-type Handler<Op extends keyof operations> = (
-	context: Context,
-	req: Request<Op>,
-	h?: ResponseToolkit<Op>,
-	err?: Error,
-) => Promise<unknown>;
-
-type HandlerCollection = {
-	[Op in keyof operations]?: Handler<Op>;
-}
+import { Handler, HandlerCollection } from "../types";
 
 /**
  * @swagger
